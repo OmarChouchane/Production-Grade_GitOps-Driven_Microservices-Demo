1734adc915ded45ccb1872f84ccb3a2fa0bec1bf
OmarChouchane <omar.ch52831@gmail.com>
2026-09-05 20:06:58 +0100

    adjusting to playground limits

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,10 +2,10 @@ resource "aws_security_group" "add_sg_eks" {
   name   = "additional-eks-sg"
   vpc_id = module.vpc.vpc_id
   ingress {
-    description = "HTTPS from bastion host"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    description     = "HTTPS from bastion host"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
     security_groups = [aws_security_group.bastion_sg.id]
   }
 
@@ -29,6 +29,10 @@ module "eks" {
   name               = "terraform-cluster"
   kubernetes_version = "1.34"
 
+  create_iam_role = false
+  iam_role_arn     = data.aws_iam_role.eks_cluster_role.arn
+
+
   addons = {
     coredns = {}
     eks-pod-identity-agent = {
@@ -47,18 +51,20 @@ module "eks" {
   enable_cluster_creator_admin_permissions = true
 
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id                        = module.vpc.vpc_id
+  subnet_ids                    = module.vpc.private_subnets
   additional_security_group_ids = [aws_security_group.add_sg_eks.id]
 
   eks_managed_node_groups = {
     example = {
+      create_iam_role = false
+      iam_role_arn    = data.aws_iam_role.eks_node_role.arn
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["c7i-flex.large"]
+      instance_types = ["t3.medium"]
 
       min_size     = 2
-      max_size     = 10
+      max_size     = 5
       desired_size = 2
     }
   }
